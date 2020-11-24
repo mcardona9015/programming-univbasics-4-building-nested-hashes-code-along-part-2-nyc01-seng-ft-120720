d07eef0b059c1fa8be3eface0d0644a8517e78a8
Michael Cardona <mcardona9015@gmail.com>
2020-11-24 16:34:01 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -25,8 +25,6 @@ def update_status
   epic_tragedy[:montague][:hero][:status] = 'dead'
   epic_tragedy[:capulet][:heroine][:status] = 'dead'
 def update_status(status)
-  epic_tragedy[:montague][:hero][:status] = status
-  epic_tragedy[:capulet][:heroine][:status] = status
 end
 
 
